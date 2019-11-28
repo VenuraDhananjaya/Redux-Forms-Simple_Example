612702fdebbcdd79dd17267cae1dba7a70126b72
Venura Dananjaya <venura.dananjaya@gmail.com>
2019-11-28 11:00:44 +0530

    Edit: login form

--- src/Componants/LoginForm/LoginForm.js
+++ src/Componants/LoginForm/LoginForm.js
@@ -5,6 +5,23 @@ import { required, email } from 'redux-form-validators'
 import renderInput from '../Application/inputfield/inputfield'
 import renderLabel from '../Application/label/label'
 
+const results = (values) => {
+    window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`)
+}
+
+let pwd_visibility = () => {
+    let visible_icon = document.getElementById("pwd_visible").className
+    if (visible_icon === "fa fa-fw fa-eye-slash") {
+        document.getElementById("pwd_visible").className = "fa fa-fw fa-eye"
+        document.getElementById("pwd_field").type = "text"
+    }
+    else if (visible_icon === "fa fa-fw fa-eye") {
+        document.getElementById("pwd_visible").className = "fa fa-fw fa-eye-slash"
+        document.getElementById("pwd_field").type = "password"
+    }
+
+}
+
 let LoginForm = ({ handleSubmit, submitting }) =>
     <div className="container">
         <div className="row justify-content-center">
@@ -12,45 +29,54 @@ let LoginForm = ({ handleSubmit, submitting }) =>
                 <div className="card text-center">
                     <div className="card-header">
                         Login Here
+                        <span style={{paddingLeft:"1%"}} className="fa fa-sign-in"></span>
                     </div>
                     <div className="card-body">
-                        <form onSubmit={handleSubmit}>
-                        <div className="row align-items-start">
-                            <div className="col-6">
-                                <Field
-                                    label={'Email'}
-                                    component={renderLabel}
-                                />
+                        <form onSubmit={handleSubmit(results)}>
+                            <div className="row align-items-start">
+                                <div className="col-6">
+                                    <Field
+                                        label={'Email'}
+                                        component={renderLabel}
+                                    />
+                                </div>
+                                <div className="col-5">
+                                    <Field
+                                        name="email"
+                                        component={renderInput}
+                                        validate={[
+                                            required({ message: "This field is required" }),
+                                            email({ message: "Not a valid email" })
+                                        ]}
+                                    />
+                                </div>
                             </div>
-                            <div className="col-6">
-                                <Field
-                                    name="email"
-                                    component={renderInput}
-                                    validate={[
-                                        required({message:"This field is required"}),
-                                        email({message:"Not a valid email"})
-                                ]}
-                                />
-                            </div>
-                        </div>
-                        <div className="row align-items-start">
-                            <div className="col-6">
-                                <Field
-                                    label={'Password'}
-                                    component={renderLabel}
-                                />
-                            </div>
-                            <div className="col-6">
-                                <Field
-                                    name="password"
-                                    field_type={'password'}
-                                    component={renderInput}
-                                    validate={required({message:"This field is required"})}
+                            <div className="row align-items-start">
+                                <div className="col-6">
+                                    <Field
+                                        label={'Password'}
+                                        component={renderLabel}
+                                    />
+                                </div>
+                                <div className="col-5">
 
-                                />
+                                    <Field
+                                        name="password"
+                                        id="pwd_field"
+                                        field_type={'password'}
+                                        component={renderInput}
+                                        validate={required({ message: "This field is required" })}
+
+                                    />
+
+                                </div>
+                                <div className="align-items-end">
+                                    <span id="pwd_visible" value="abc" style={{ marginTop: "90%" }} className="fa fa-fw fa-eye-slash" onClick={pwd_visibility}></span>
+                                </div>
+
+
                             </div>
-                        </div>
-                        <button type="submit" className="btn btn-primary" disabled={submitting}>Login</button>
+                            <button className="btn btn-primary" type="submit"  disabled={submitting}>Login</button>
                         </form>
                     </div>
                     <div className="card-footer text-muted">
--- src/Componants/RegisterForm/RegistrationForm.js
+++ src/Componants/RegisterForm/RegistrationForm.js
@@ -5,52 +5,81 @@ import { required, format, confirmation, email, length } from 'redux-form-valida
 import options from './options'
 import renderInput from '../Application/inputfield/inputfield'
 import renderSelect from '../Application/dropdown/dropdown'
+import renderLabel from '../Application/label/label'
 
 const results = (values) => {
     window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`)
 }
-const renderLabel = ({ label }) =>
-    <label>{label}</label>
+
+let pwd_visibility = () => {
+    let visible_icon = document.getElementById("pwd_visible").className
+    let normal_eye = "fa fa-eye"
+    let eye_slash = "fa fa-eye-slash"
+    if (visible_icon === eye_slash) {
+        document.getElementById("pwd_visible").className = normal_eye
+        document.getElementById("registration_pwd").type = "text"
+    }
+    else if (visible_icon === normal_eye) {
+        document.getElementById("pwd_visible").className = eye_slash
+        document.getElementById("registration_pwd").type = "password"
+    }
+
+}
+
+let conf_pwd_visibility = () => {
+    let visible_icon = document.getElementById("conf_pwd_visible").className
+    let normal_eye = "fa fa-eye"
+    let eye_slash = "fa fa-eye-slash"
+    if (visible_icon === eye_slash) {
+        document.getElementById("conf_pwd_visible").className = normal_eye
+        document.getElementById("registration_conf_pwd").type = "text"
+    }
+    else if (visible_icon === normal_eye) {
+        document.getElementById("conf_pwd_visible").className = eye_slash
+        document.getElementById("registration_conf_pwd").type = "password"
+    }
+
+}
 
 
 let RegistrationForm = ({ handleSubmit, submitting }) =>
     <div className="container">
         <div className="row justify-content-center">
             <div className="col-6">
-                <div class="card text-center">
-                    <div class="card-header">
+                <div className="card text-center">
+                    <div className="card-header ">
                         Registration Form
                     </div>
                     <form onSubmit={handleSubmit(results)}>
-                        <div class="card-body">
+                        <div className="card-body">
 
                             <div className="row align-items-start">
-                                <div className="col-6">
+                                <div className="col-5">
                                     <Field
                                         label={'First Name'}
                                         component={renderLabel}
                                     />
                                 </div>
-                                <div className="col-6">
+                                <div className="col-5">
                                     <Field
                                         name="first_name"
                                         component={renderInput}
                                         validate={[
                                             required({ message: "This field is required" }),
                                             format({ with: /^[a-zA-z ]*$/i, message: "Invalid Name" }),
-                                            length({ min: 2 })
+                                            length({ min: 3, message: "Invalid Name" })
                                         ]}
                                     />
                                 </div>
                             </div>
                             <div className="row align-items-start">
-                                <div className="col-6">
+                                <div className="col-5">
                                     <Field
                                         label={'Last name'}
                                         component={renderLabel}
                                     />
                                 </div>
-                                <div className="col-6">
+                                <div className="col-5">
                                     <Field
                                         name="last_name"
                                         component={renderInput}
@@ -63,13 +92,13 @@ let RegistrationForm = ({ handleSubmit, submitting }) =>
                                 </div>
                             </div>
                             <div className="row align-items-start">
-                                <div className="col-6">
+                                <div className="col-5">
                                     <Field
                                         label={'Email'}
                                         component={renderLabel}
                                     />
                                 </div>
-                                <div className="col-6">
+                                <div className="col-5">
                                     <Field
                                         name="email"
                                         component={renderInput}
@@ -81,13 +110,13 @@ let RegistrationForm = ({ handleSubmit, submitting }) =>
                                 </div>
                             </div>
                             <div className="row align-items-start">
-                                <div className="col-6">
+                                <div className="col-5">
                                     <Field
                                         label={'District'}
                                         component={renderLabel}
                                     />
                                 </div>
-                                <div className="col-6">
+                                <div className="col-5">
                                     <Field
                                         name="option"
                                         component={renderSelect}
@@ -102,33 +131,38 @@ let RegistrationForm = ({ handleSubmit, submitting }) =>
                                 </div>
                             </div>
                             <div className="row align-items-start">
-                                <div className="col-6">
+                                <div className="col-5">
                                     <Field
                                         label={'Password'}
                                         component={renderLabel}
                                     />
                                 </div>
-                                <div className="col-6">
+                                <div className="col-5">
                                     <Field
+                                        id="registration_pwd"
                                         field_type="password"
                                         name="password"
                                         field_ component={renderInput}
                                         validate={[
                                             required({ message: "This field is required" }),
                                             length({ min: 6, max: 10, message: "Password should between 6-10 chars" })
-                                            ]}
+                                        ]}
                                     />
+                                </div>
+                                <div className="align-items-end">
+                                    <span id="pwd_visible" style={{ marginTop: "90%" }} className="fa fa-eye-slash" onClick={pwd_visibility} ></span>
                                 </div>
                             </div>
                             <div className="row align-items-start">
-                                <div className="col-6">
+                                <div className="col-5">
                                     <Field
                                         label={'Confirm Password'}
                                         component={renderLabel}
                                     />
                                 </div>
-                                <div className="col-6">
+                                <div className="col-5">
                                     <Field
+                                        id="registration_conf_pwd"
                                         field_type="password"
                                         name="conf_password"
                                         component={renderInput}
@@ -138,12 +172,16 @@ let RegistrationForm = ({ handleSubmit, submitting }) =>
                                         ]}
                                     />
                                 </div>
+                                <div className="align-items-end">
+                                    <span id="conf_pwd_visible" style={{ marginTop: "90%" }} className="fa fa-eye-slash" onClick={conf_pwd_visibility}></span>
+                                </div>
                             </div>
+
                             <button className="btn btn-primary" type="submit" disabled={submitting}>Submit</button>
 
                         </div>
                     </form>
-                    <div class="card-footer text-muted">
+                    <div className="card-footer text-muted">
                         Already a member? <a href="/login">Login here</a>
                     </div>
                 </div>
